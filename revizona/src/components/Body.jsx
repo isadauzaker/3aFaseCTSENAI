63d85f3c9a76d3535bde6c97eea8ea93828625fd
isadauzaker <isadoravictor7@gmail.com>
2025-02-06 17:17:17 -0300

    Revisão!

---
+++ revizona/src/components/Body.jsx
@@ -0,0 +1,13 @@
+import './Body.css'
+import Demo1 from './Demo1'
+
+function Body() {
+  return (
+    <div className='container-body'>
+        <h1>Empresa de Logos</h1>
+        <Demo1 />
+    </div>
+  )
+}
+
+export default Body
---
+++ revizona/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import './Header.css'
+
+function Header() {
+  return (
+    <div className="container-header">
+        <p>LoGo</p>
+        <p>Link 1</p>
+        <p>Link 2</p>
+        <p>Link 5</p>
+    </div>
+  )
+}
+
+export default Header
